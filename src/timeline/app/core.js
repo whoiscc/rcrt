f2ada4cdde682539d3a0d8632ef19ceeaaeba15a
whoiscc <correctizer@gmail.com>
2021-11-04 09:31:09 +0800

    Wip timeline

--- src/timeline/app/core.js
+++ src/timeline/app/core.js
@@ -8,30 +8,40 @@ function startApp() {
 
 function startAppCallback() {
     const path = (new URL(window.location.href)).hash;
-
     fetch('db/meta.json').then(resp => resp.json()).then(meta => {
         const page = 
-            path.length === 0 ? 
+            path === "" ? 
                 createFrontPage(meta) : 
             path.startsWith('#_hidden/') ?
                 createHiddenPage(meta, path.slice(9)) :
                 createPage(meta, path.slice(1));
         document.body.innerHTML = '';
         document.body.appendChild(page);
-        document.title = path.length === 0 ? 'Rigid Timeline' : '(WIP)';
+        document.title = 'Rigid Timeline';  // TODO
     });
 }
 
+const FONT_SANS = 'Noto Sans CJK SC';
+const FONT_MONO = 'Noto Sans Mono';
+
 function addPageStyle(node) {
+    node.style.display = 'block';
     node.style.margin = '0 auto';
     node.style.maxWidth = '800px';
 }
 
-const FONT_SANS = 'Noto Sans CJK SC';
-
 function addParagraphStyle(node) {
     node.style.fontFamily = FONT_SANS;
     node.style.fontSize = '1.2em';
+}
+
+function addAttachingStyle(node) {
+    node.style.marginTop = '-1em';
+}
+
+function addSeriesStyle(node) {
+    node.style.fontFamily = FONT_SANS;
+    node.style.fontSize = '0.9em';
 }
 
 function addTimeStyle(node) {
@@ -45,8 +55,14 @@ function addTitleStyle(node) {
 }
 
 function addRefStyle(node) {
+    node.style.fontFamily = FONT_MONO;
+    node.style.fontSize = '0.85em';
+}
+
+function addLinkStyle(node) {
     node.style.fontFamily = FONT_SANS;
     node.style.fontSize = '0.95em';
+    node.style.color = 'black';
 }
 
 function getTimeString(time) {
@@ -56,11 +72,24 @@ function getTimeString(time) {
 }
 
 function createFrontPage(meta) {
-    //
+    const testFontNode = document.createElement('div');
+    document.body.appendChild(testFontNode);
+    testFontNode.style.fontFamily = FONT_SANS;
+    const actualFont = window.getComputedStyle(testFontNode).fontFamily;
+    testFontNode.style.fontFamily = FONT_MONO;
+    const actualMonoFont = window.getComputedStyle(testFontNode).fontFamily;
+    document.body.removeChild(testFontNode);
+    if (!FONT_SANS.includes(actualFont) || !FONT_MONO.includes(actualMonoFont)) {
+        console.log('TODO prompt install fonts');
+    }
+
     return new Text('This is main page');
 }
 
 function createPage(meta, identifier) {
+    if (meta[identifier] === undefined) {
+        return create404Page(meta);
+    }
     switch (meta[identifier].type) {
         case 'link':
             return createLinkPage(meta, identifier);
@@ -74,8 +103,14 @@ function createPage(meta, identifier) {
     }
 }
 
+function create404Page(meta) {
+    return new Text('404 NOT FOUND');
+}
+
 function createLinkPage(meta, identifier) {
     const node = document.createElement('a');
+    addPageStyle(node);
+    addLinkStyle(node);
     node.href = node.text = meta[identifier].url;
     return node;
 }
@@ -101,13 +136,26 @@ function createArticlePage(meta, identifier) {
     title.innerText = meta[identifier].title;
     addTitleStyle(title);
     node.appendChild(title);
+    if (meta[identifier].series !== "") {
+        const series = document.createElement('p');
+        addSeriesStyle(series);
+        addAttachingStyle(series);
+        series.appendChild(new Text('系列 / '));
+        const seriesA = document.createElement('a');
+        addLinkStyle(seriesA);
+        seriesA.innerText = meta[identifier].series;
+        seriesA.href = '#';  // TODO
+        series.appendChild(seriesA);
+        node.appendChild(series);
+    }
     const time = document.createElement('p');
     addTimeStyle(time);
+    addAttachingStyle(time);
     time.innerText = getTimeString(meta[identifier].time);
     node.appendChild(time);
     fetch(`db/${identifier}.txt`).then(resp => resp.text()).then(content => {
         for (let paragraph of content.split('\n\n')) {
-            if (paragraph.length === 0) {
+            if (paragraph === "") {
                 continue;
             }
             const p = document.createElement('p');
@@ -115,10 +163,14 @@ function createArticlePage(meta, identifier) {
             for (let match of paragraph.matchAll(/(.*?)((\[.*?\]#\w{6})|$)/g)) {
                 const [text, ref] = [match[1], match[2]];
                 p.appendChild(new Text(text));
-                if (ref.length !== 0) {
+                if (ref !== "") {
                     const a = document.createElement('a');
-                    addRefStyle(a);
-                    a.innerText = ref;
+                    addLinkStyle(a);
+                    a.appendChild(new Text(ref.slice(0, ref.length - 7)));
+                    const aRef = document.createElement('span');
+                    aRef.innerText = ref.slice(ref.length - 7);
+                    addRefStyle(aRef);
+                    a.appendChild(aRef);
                     a.href = ref.slice(ref.length - 7);
                     p.appendChild(a);
                 }
